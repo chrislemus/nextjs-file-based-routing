768eb159aaadf04e1c032e229cf2cf850bbf0581
chrislemus <icrislemus@gmail.com>
2021-04-08 13:05:45 -0400

    adding dynamic nested routes

---
+++ pages/clients/[id]/[clientProjectId].js
@@ -0,0 +1,7 @@
+export default function SelectedClientProjectPage() {
+  return (
+    <div>
+      <h1>The Project Page for Specific project. Specific client</h1>
+    </div>
+  );
+}
---
+++ pages/clients/[id]/index.js
@@ -0,0 +1,7 @@
+export default function ClientProjectPage() {
+  return (
+    <div>
+      <h1>The Projects of a given clients</h1>
+    </div>
+  );
+}
